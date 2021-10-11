8d3a4ba59b608334cdcce0118c60052f16f8c18b
JailBreaker <64787457+JailBreakerTV@users.noreply.github.com>
2021-10-11 19:23:17 +0200

    access TranslatorFactory from Client#display and translate the input value

--- src/org/hbrs/se1/ws21/uebung1/view/Client.java
+++ src/org/hbrs/se1/ws21/uebung1/view/Client.java
@@ -1,21 +1,23 @@
 package org.hbrs.se1.ws21.uebung1.view;
 
+import org.hbrs.se1.ws21.uebung1.control.Translator;
+import org.hbrs.se1.ws21.uebung1.control.factory.TranslatorFactory;
+
 public class Client {
 
-	/*
-	 * Methode zur Ausgabe einer Zahl auf der Console
-	 */
-	public void display( int aNumber ){
-		// In dieser Methode soll die Methode translateNumber 
-		// mit dem übergegebenen Wert der Variable aNumber 
-		// aufgerufen werden.
-		//
-		// Strenge Implementierung gegen das Interface Translator gewuenscht!
-
-		System.out.println("Das Ergebnis der Berechnung: " +
-				"[das Ergebnis an dieser Stelle]"  );
-
-	}
+    /*
+     * Methode zur Ausgabe einer Zahl auf der Console
+     */
+    public void display(int aNumber) {
+        // In dieser Methode soll die Methode translateNumber
+        // mit dem übergegebenen Wert der Variable aNumber
+        // aufgerufen werden.
+        //
+        // Strenge Implementierung gegen das Interface Translator gewuenscht!
+        final Translator translator = TranslatorFactory.create();
+        final String translated = translator.translateNumber(aNumber);
+        System.out.printf("Das Ergebnis der Berechnung: %s%n", translated);
+    }
 }
 
 
