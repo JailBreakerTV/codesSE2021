568adb3da8e740a5ccb7a756c2950db4e07fb940
JailBreaker <64787457+JailBreakerTV@users.noreply.github.com>
2021-10-20 19:01:45 +0200

    code refactoring and stream-strategy implementation

--- src/main/java/org/hbrs/se1/ws21/uebung3/persistence/PersistenceStrategy.java
+++ src/main/java/org/hbrs/se1/ws21/uebung3/persistence/PersistenceStrategy.java
@@ -7,7 +7,7 @@ import java.util.List;
  * Each concrete algorithm (i.e. strategy) must implement this method
  * This interface corresponds to the abstract strategy w.r.t. to the
  * Strategy Design Pattern (GoF).
- *
+ * <p>
  * The following protocol applies:
  * 1. openConnection
  * 2. { load | save }  (many times)
@@ -16,8 +16,27 @@ import java.util.List;
  * @param <E>
  */
 public interface PersistenceStrategy<E> {
-    public void openConnection() throws PersistenceException;
-    public void closeConnection() throws PersistenceException;
-    public void save(List<E> member) throws PersistenceException;
-    public List<E> load() throws PersistenceException;
+    /**
+     * Method for opening the connection to a stream (here: Input- and Output-Stream)
+     * In case of having problems while opening the streams, leave the code in methods load
+     * and save
+     */
+    void openConnection() throws PersistenceException;
+
+    /**
+     * Method for closing the connections to a stream
+     */
+    void closeConnection() throws PersistenceException;
+
+    /**
+     * Method for saving a list of Member-objects to a disk (HDD)
+     */
+    void save(List<E> member) throws PersistenceException;
+
+    /**
+     * Method for loading a list of Member-objects from a disk (HDD)
+     * Some coding examples come for free :-)
+     * Take also a look at the import statements above ;-!
+     */
+    List<E> load() throws PersistenceException;
 }
--- src/main/java/org/hbrs/se1/ws21/uebung3/persistence/PersistenceStrategyStream.java
+++ src/main/java/org/hbrs/se1/ws21/uebung3/persistence/PersistenceStrategyStream.java
@@ -1,73 +1,82 @@
 package org.hbrs.se1.ws21.uebung3.persistence;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+import static org.hbrs.se1.ws21.uebung3.persistence.PersistenceException.ExceptionType.ConnectionNotAvailable;
+
+@NoArgsConstructor
 public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Member> {
 
+    @Getter
+    // Backdoor method used only for testing purposes, if the location should be changed in a Unit-Test
+    // Example: Location is a directory (Streams do not like directories, so try this out ;-)!
+    @Setter
     // URL of file, in which the objects are stored
     private String location = "objects.ser";
 
-    // Backdoor method used only for testing purposes, if the location should be changed in a Unit-Test
-    // Example: Location is a directory (Streams do not like directories, so try this out ;-)!
-    public void setLocation(String location) {
+    public PersistenceStrategyStream(String location) {
         this.location = location;
     }
 
     @Override
-    /**
-     * Method for opening the connection to a stream (here: Input- and Output-Stream)
-     * In case of having problems while opening the streams, leave the code in methods load
-     * and save
-     */
     public void openConnection() throws PersistenceException {
-
+        try {
+            final Path path = Paths.get(this.location);
+            if (Files.notExists(path)) {
+                Files.createFile(path);
+            }
+            if (Files.isDirectory(path)) {
+                throw new PersistenceException(ConnectionNotAvailable, "Given File can not be a directory");
+            }
+        } catch (IOException e) {
+            throw new PersistenceException(ConnectionNotAvailable, e.getMessage());
+        }
     }
 
     @Override
-    /**
-     * Method for closing the connections to a stream
-     */
     public void closeConnection() throws PersistenceException {
 
     }
 
     @Override
-    /**
-     * Method for saving a list of Member-objects to a disk (HDD)
-     */
-    public void save(List<Member> member) throws PersistenceException  {
-
+    public void save(List<Member> member) throws PersistenceException {
+        if (!(member instanceof Serializable)) {
+            throw new PersistenceException(
+                    ConnectionNotAvailable,
+                    "Die Listen-Implementation muss das Interface Serializable implementieren"
+            );
+        }
+        try (
+                final FileOutputStream fout = new FileOutputStream(this.location);
+                final ObjectOutputStream oout = new ObjectOutputStream(fout)
+        ) {
+            oout.writeObject(member);
+        } catch (IOException e) {
+            throw new PersistenceException(ConnectionNotAvailable, e.getMessage());
+        }
     }
 
     @Override
-    /**
-     * Method for loading a list of Member-objects from a disk (HDD)
-     * Some coding examples come for free :-)
-     * Take also a look at the import statements above ;-!
-     */
-    public List<Member> load() throws PersistenceException  {
-        // Some Coding hints ;-)
-        // ObjectInputStream ois = null;
-        // FileInputStream fis = null;
-        // List<...> newListe =  null;
-        //
-        // Initiating the Stream (can also be moved to method openConnection()... ;-)
-        // fis = new FileInputStream( " a location to a file" );
-        // ois = new ObjectInputStream(fis);
-
-        // Reading and extracting the list (try .. catch ommitted here)
-        // Object obj = ois.readObject();
-
-        // if (obj instanceof List<?>) {
-        //       newListe = (List) obj;
-        // return newListe
-
-        // and finally close the streams (guess where this could be...?)
-        return null;
+    public List<Member> load() throws PersistenceException {
+        try (
+                final FileInputStream fin = new FileInputStream(this.location);
+                final ObjectInputStream oin = new ObjectInputStream(fin)
+        ) {
+            final Object rawMembers = oin.readObject();
+            if (!(rawMembers instanceof List)) {
+                return null;
+            }
+            return (List<Member>) rawMembers;
+        } catch (IOException | ClassNotFoundException e) {
+            throw new PersistenceException(ConnectionNotAvailable, e.getMessage());
+        }
     }
 }
