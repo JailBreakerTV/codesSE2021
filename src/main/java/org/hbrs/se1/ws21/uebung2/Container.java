8fdfa9a1b90a664c539afd108a0a90e56de53cb8
JailBreaker <64787457+JailBreakerTV@users.noreply.github.com>
2021-10-20 19:14:42 +0200

    apply 2nd change request

--- src/main/java/org/hbrs/se1/ws21/uebung2/Container.java
+++ src/main/java/org/hbrs/se1/ws21/uebung2/Container.java
@@ -2,6 +2,9 @@ package org.hbrs.se1.ws21.uebung2;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hbrs.se1.ws21.uebung3.persistence.PersistenceException;
+import org.hbrs.se1.ws21.uebung3.persistence.PersistenceStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +15,27 @@ public class Container {
 
     private final List<Member> members = new ArrayList<>();
 
+    @Setter
+    private PersistenceStrategy<Member> strategy;
+
     public static Container getContainer() {
         return CONTAINER;
+    }
+
+    public void store() throws PersistenceException {
+        if (this.strategy == null) {
+            throw new NullPointerException("PersistenceStrategy is null");
+        }
+        this.strategy.save(this.members);
+    }
+
+    public void load() throws PersistenceException {
+        if (this.strategy == null) {
+            throw new NullPointerException("PersistenceStrategy is null");
+        }
+        final List<Member> loadedMembers = this.strategy.load();
+        this.members.removeIf(member -> loadedMembers.stream().anyMatch(member::equals));
+        this.members.addAll(loadedMembers);
     }
 
     public void addMember(Member member) throws ContainerException {
