65b4cd5bab2c430a27906f346c5097e216ac8aee
JailBreaker <64787457+JailBreakerTV@users.noreply.github.com>
2021-10-20 23:34:47 +0200

    implement load and store functions

--- src/main/java/org/hbrs/se1/ws21/uebung3/persistence/PersistenceStrategyStream.java
+++ src/main/java/org/hbrs/se1/ws21/uebung3/persistence/PersistenceStrategyStream.java
@@ -5,12 +5,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
 
-import static org.hbrs.se1.ws21.uebung3.persistence.PersistenceException.ExceptionType.ConnectionNotAvailable;
+import static org.hbrs.se1.ws21.uebung3.persistence.PersistenceException.ExceptionType.*;
 
 @NoArgsConstructor
 public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Member> {
@@ -31,52 +29,74 @@ public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Me
         try {
             final Path path = Paths.get(this.location);
             if (Files.notExists(path)) {
-                Files.createFile(path);
+                try {
+                    Files.createFile(path);
+                } catch (FileAlreadyExistsException e) {
+                    throw new PersistenceException(OutputFileAlreadyExist, e.getMessage());
+                } catch (Throwable t) {
+                    throw new PersistenceException(OutputFileCouldNotBeCreated, t.getMessage());
+                }
             }
             if (Files.isDirectory(path)) {
-                throw new PersistenceException(ConnectionNotAvailable, "Given File can not be a directory");
+                throw new PersistenceException(OutputFileCanNotBeDirectory, "Given File can not be a directory");
             }
-        } catch (IOException e) {
-            throw new PersistenceException(ConnectionNotAvailable, e.getMessage());
+        } catch (InvalidPathException e) {
+            throw new PersistenceException(OutputFilePathIsInvalid, e.getMessage());
         }
     }
 
     @Override
     public void closeConnection() throws PersistenceException {
-
+        throw new PersistenceException(ImplementationNotAvailable, "There is no implementation available yet");
     }
 
     @Override
     public void save(List<Member> member) throws PersistenceException {
         if (!(member instanceof Serializable)) {
             throw new PersistenceException(
-                    ConnectionNotAvailable,
-                    "Die Listen-Implementation muss das Interface Serializable implementieren"
+                    ValueMustImplementSerializable,
+                    "The value to be saved must be an implementation of the Serializable interface"
             );
         }
-        try (
-                final FileOutputStream fout = new FileOutputStream(this.location);
-                final ObjectOutputStream oout = new ObjectOutputStream(fout)
-        ) {
-            oout.writeObject(member);
+        final Serializable serializable = (Serializable) member;
+        try {
+            this.write(serializable);
+        } catch (FileNotFoundException e) {
+            throw new PersistenceException(OutputFileNotExisting, "There is no file which can be edited");
         } catch (IOException e) {
-            throw new PersistenceException(ConnectionNotAvailable, e.getMessage());
+            e.printStackTrace();
+            throw new PersistenceException(ValueCouldNotBeSaved, e.getMessage());
         }
     }
 
     @Override
     public List<Member> load() throws PersistenceException {
-        try (
-                final FileInputStream fin = new FileInputStream(this.location);
-                final ObjectInputStream oin = new ObjectInputStream(fin)
-        ) {
-            final Object rawMembers = oin.readObject();
-            if (!(rawMembers instanceof List)) {
-                return null;
+        try {
+            return this.read();
+        } catch (ClassCastException e) {
+            throw new PersistenceException(ValueCouldNotBeCasted, e.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new PersistenceException(ValueClassCouldNotBeFound, e.getMessage());
+        } catch (IOException e) {
+            throw new PersistenceException(ValueCouldNotBeFetched, e.getMessage());
+        }
+    }
+
+    private void write(Serializable serializable) throws IOException {
+        try (final FileOutputStream fileOut = new FileOutputStream(this.location);
+             final ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
+            objectOut.writeObject(serializable);
+        }
+    }
+
+    private <T> T read() throws IOException, ClassNotFoundException, ClassCastException, NullPointerException {
+        try (final FileInputStream fileIn = new FileInputStream(this.location);
+             final ObjectInputStream objectIn = new ObjectInputStream(fileIn)) {
+            final Object raw = objectIn.readObject();
+            if (raw == null) {
+                throw new NullPointerException("Raw value from ObjectInputStream is null");
             }
-            return (List<Member>) rawMembers;
-        } catch (IOException | ClassNotFoundException e) {
-            throw new PersistenceException(ConnectionNotAvailable, e.getMessage());
+            return (T) raw;
         }
     }
 }
