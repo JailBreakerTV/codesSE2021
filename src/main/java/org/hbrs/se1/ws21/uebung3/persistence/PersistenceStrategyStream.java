394cb60419e3c655800f2f8507844f892b245f53
JailBreaker <64787457+JailBreakerTV@users.noreply.github.com>
2021-10-26 23:52:02 +0200

    add some documentation

--- src/main/java/org/hbrs/se1/ws21/uebung3/persistence/PersistenceStrategyStream.java
+++ src/main/java/org/hbrs/se1/ws21/uebung3/persistence/PersistenceStrategyStream.java
@@ -10,8 +10,13 @@ import java.util.List;
 
 import static org.hbrs.se1.ws21.uebung3.persistence.PersistenceException.ExceptionType.*;
 
+/**
+ * This strategy class takes care of storing the content within a stream
+ *
+ * @param <Member> the type of data to be stored
+ */
 @NoArgsConstructor
-public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Member> {
+public final class PersistenceStrategyStream<Member> implements PersistenceStrategy<Member> {
 
     @Getter
     // Backdoor method used only for testing purposes, if the location should be changed in a Unit-Test
@@ -86,6 +91,16 @@ public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Me
         }
     }
 
+    /**
+     * This function writes a {@link Serializable} into a specific {@link OutputStream} implementation
+     * which is responsible for saving the content into a file.
+     *
+     * @param serializable which should be saved into a file
+     * @throws IOException if an I/O error occurs while writing stream header
+     * @see FileOutputStream
+     * @see ObjectOutputStream
+     * @see ObjectOutputStream#writeObject(Object)
+     */
     private void write(Serializable serializable) throws IOException {
         try (final FileOutputStream fileOut = new FileOutputStream(this.location);
              final ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
@@ -93,6 +108,18 @@ public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Me
         }
     }
 
+    /**
+     * This function reads the contents of the given file and
+     * tries to cast this extracted value to the type of the given generic
+     *
+     * @param <T> generic, which specifies the type of the read data
+     * @return The value which was extracted and casted
+     * @throws IOException            if an I/O error occurs while reading stream header
+     * @throws ClassNotFoundException Class of a serialized object cannot be found.
+     * @throws ClassCastException     if the extracted value is not an instance of the given generic type
+     * @throws NullPointerException   if the extracted value is null
+     * @see ObjectInputStream#readObject()
+     */
     private <T> T read() throws IOException, ClassNotFoundException, ClassCastException, NullPointerException {
         try (final FileInputStream fileIn = new FileInputStream(this.location);
              final ObjectInputStream objectIn = new ObjectInputStream(fileIn)) {
