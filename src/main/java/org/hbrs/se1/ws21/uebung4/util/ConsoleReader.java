8ccfb41b95165d0869b3d5241c2d17ac81103f41
JailBreaker <64787457+JailBreakerTV@users.noreply.github.com>
2021-11-09 00:17:48 +0100

    refactor: move ConsoleReader into a separate util package refactor: rename ConsoleReader#read into ConsoleReader#start

--- src/main/java/org/hbrs/se1/ws21/uebung4/util/ConsoleReader.java
+++ src/main/java/org/hbrs/se1/ws21/uebung4/util/ConsoleReader.java
@@ -1,21 +1,21 @@
-package org.hbrs.se1.ws21.uebung4;
+package org.hbrs.se1.ws21.uebung4.util;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.hbrs.se1.ws21.uebung4.SprintPlanningMessages;
 
 import java.util.Scanner;
 import java.util.function.Consumer;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ConsoleReader {
-    public static void read(Consumer<String> consumer) {
+    public static void start(Consumer<String> consumer) {
         new Thread(() -> {
             final Scanner scanner = new Scanner(System.in);
-            System.out.print(SprintPlanningMessages.PREFIX);
+            System.out.print(SprintPlanningMessages.PROMPT_PREFIX);
             while (scanner.hasNext()) {
                 consumer.accept(scanner.nextLine());
-                System.out.println();
-                System.out.print(SprintPlanningMessages.PREFIX);
+                System.out.print(SprintPlanningMessages.PROMPT_PREFIX);
             }
         }).start();
     }
