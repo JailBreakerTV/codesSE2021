4b0592ddaef300b2b98273ccba3fbb48e5ace3c4
JailBreaker <64787457+JailBreakerTV@users.noreply.github.com>
2021-11-09 00:32:39 +0100

    feat: provide joinToString function for collections

--- src/main/java/org/hbrs/se1/ws21/uebung4/util/CollectionUtil.java
+++ src/main/java/org/hbrs/se1/ws21/uebung4/util/CollectionUtil.java
@@ -3,6 +3,7 @@ package org.hbrs.se1.ws21.uebung4.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -11,5 +12,16 @@ import java.util.stream.Collectors;
 public final class CollectionUtil {
     public static List<String> sorted(Set<String> set) {
         return set.stream().sorted().collect(Collectors.toList());
+    }
+
+    public static String joinToString(Collection<String> collection, String delimiter) {
+        return joinToString(collection, delimiter, "");
+    }
+
+    public static String joinToString(Collection<String> collection, String delimiter, String def) {
+        if (collection.isEmpty()) {
+            return def;
+        }
+        return String.join(delimiter, collection);
     }
 }
