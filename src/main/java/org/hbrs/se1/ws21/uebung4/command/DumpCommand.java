9152c76dc046851b4ac7a2609e8985d81cd9ee83
JailBreaker <64787457+JailBreakerTV@users.noreply.github.com>
2021-11-09 01:07:10 +0100

    feat: add exit and dump command

---
+++ src/main/java/org/hbrs/se1/ws21/uebung4/command/DumpCommand.java
@@ -0,0 +1,57 @@
+package org.hbrs.se1.ws21.uebung4.command;
+
+import org.hbrs.se1.ws21.uebung2.Container;
+import org.hbrs.se1.ws21.uebung4.employee.Employee;
+import org.hbrs.se1.ws21.uebung4.expertise.Expertise;
+import org.hbrs.se1.ws21.uebung4.util.table.TablePrinter;
+import org.hbrs.se1.ws21.uebung4.util.table.TablePrinterException;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.hbrs.se1.ws21.uebung4.util.CollectionUtil.joinToString;
+
+public final class DumpCommand extends ConsoleCommand {
+    private final Container<Employee> employeeContainer;
+
+    public DumpCommand(Container<Employee> employeeContainer) {
+        super("dump", "Gibt alle vorhandene Mitarbeiter aus", Collections.singleton("ausgabe"));
+        this.employeeContainer = employeeContainer;
+    }
+
+    @Override
+    public void execute(String[] args, CommandParameters parameters) {
+        final List<Employee> employees = this.employeeContainer.getCurrentList();
+        if (employees.isEmpty()) {
+            System.out.println("Aktuell sind keine Mitarbeiter registriert");
+            return;
+        }
+        final List<String> ids = employees.stream().map(Employee::getId).map(Object::toString).collect(Collectors.toList());
+        final List<String> firstNames = employees.stream().map(Employee::getFirstName).collect(Collectors.toList());
+        final List<String> lastNames = employees.stream().map(Employee::getLastName).collect(Collectors.toList());
+        final List<String> departments = employees.stream().map(Employee::getDepartment).collect(Collectors.toList());
+        final List<String> expertises = employees.stream().map(employee ->
+                joinToString(
+                        employee.getExpertises().stream().map(Expertise::getTitle).collect(Collectors.toList()),
+                        ", ",
+                        "-"
+                )
+        ).collect(Collectors.toList());
+
+        final Map<String, List<String>> tableContent = new HashMap<>() {{
+            put("Id", ids);
+            put("Firstname", firstNames);
+            put("Lastname", lastNames);
+            put("Department", departments);
+            put("Expertises", expertises);
+        }};
+        try {
+            TablePrinter.printTable(tableContent);
+        } catch (TablePrinterException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/hbrs/se1/ws21/uebung4/command/ExitCommand.java
@@ -0,0 +1,15 @@
+package org.hbrs.se1.ws21.uebung4.command;
+
+import java.util.Collections;
+
+public class ExitCommand extends ConsoleCommand {
+    public ExitCommand() {
+        super("exit", "Schaltet das Programm aus", Collections.emptySet());
+    }
+
+    @Override
+    public void execute(String[] args, CommandParameters parameters) {
+        System.out.println("Programm wird ausgeschaltet, bis dann!");
+        System.exit(0);
+    }
+}
