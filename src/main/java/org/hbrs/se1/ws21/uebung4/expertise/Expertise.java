ae713b214cbcb292bc37b975c825e76bd8b75e71
JailBreaker <64787457+JailBreakerTV@users.noreply.github.com>
2021-11-09 01:06:12 +0100

    fix: mark Expertise with the Serializable interface fix: let Expertise implement Comparable

--- src/main/java/org/hbrs/se1/ws21/uebung4/expertise/Expertise.java
+++ src/main/java/org/hbrs/se1/ws21/uebung4/expertise/Expertise.java
@@ -2,14 +2,22 @@ package org.hbrs.se1.ws21.uebung4.expertise;
 
 import lombok.Data;
 import org.hbrs.se1.ws21.uebung2.IdAware;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
 
 @Data
-public final class Expertise implements IdAware {
+public final class Expertise implements IdAware, Serializable, Comparable<Expertise> {
     private final Integer id;
     private final String title;
 
     @Override
     public Integer getId() {
         return this.id;
+    }
+
+    @Override
+    public int compareTo(@NotNull Expertise expertise) {
+        return this.id.compareTo(expertise.id);
     }
 }
