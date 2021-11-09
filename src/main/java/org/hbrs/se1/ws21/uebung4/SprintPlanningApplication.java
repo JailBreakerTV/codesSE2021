adc7b74d62946a9d8d298fecef513aac684a6ffc
JailBreaker <64787457+JailBreakerTV@users.noreply.github.com>
2021-11-09 01:06:40 +0100

    fix: extract functions from the main function into separate tiny functions

--- src/main/java/org/hbrs/se1/ws21/uebung4/SprintPlanningApplication.java
+++ src/main/java/org/hbrs/se1/ws21/uebung4/SprintPlanningApplication.java
@@ -3,29 +3,50 @@ package org.hbrs.se1.ws21.uebung4;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.hbrs.se1.ws21.uebung2.Container;
+import org.hbrs.se1.ws21.uebung3.persistence.PersistenceException;
+import org.hbrs.se1.ws21.uebung3.persistence.PersistenceStrategy;
+import org.hbrs.se1.ws21.uebung3.persistence.PersistenceStrategyStream;
 import org.hbrs.se1.ws21.uebung4.command.*;
 import org.hbrs.se1.ws21.uebung4.employee.Employee;
 import org.hbrs.se1.ws21.uebung4.employee.EmployeeContainer;
 import org.hbrs.se1.ws21.uebung4.employee.EmployeeService;
 import org.hbrs.se1.ws21.uebung4.employee.EmployeeServiceImpl;
 import org.hbrs.se1.ws21.uebung4.util.ConsoleReader;
-import org.hbrs.se1.ws21.uebung4.util.table.TablePrinter;
-import org.hbrs.se1.ws21.uebung4.util.table.TablePrinterException;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SprintPlanningApplication {
     public static void main(String[] args) {
         System.out.println(SprintPlanningMessages.STARTUP_MESSAGE);
+        registerCommands();
+        handleCommandInputs();
+        registerShutdownHook();
+    }
+
+    private static void registerCommands() {
         final Container<Employee> employeeContainer = EmployeeContainer.getInstance();
+        final PersistenceStrategy<Employee> strategy = new PersistenceStrategyStream<>("employees.dat");
+        try {
+            strategy.openConnection();
+            employeeContainer.setStrategy(strategy);
+            employeeContainer.load();
+        } catch (PersistenceException e) {
+            e.printStackTrace();
+        }
         final EmployeeService employeeService = new EmployeeServiceImpl(employeeContainer);
-        CommandRegistry.register(new EnterEmployeeCommand(employeeService, employeeContainer));
         CommandRegistry.register(new HelpCommand());
-        defaultTablePrint();
+        CommandRegistry.register(new ExitCommand());
+        CommandRegistry.register(new DumpCommand(employeeContainer));
+        CommandRegistry.register(new EnterEmployeeCommand(employeeService, employeeContainer));
+    }
+
+    /*
+    enter -id 1 -firstname Heinz -lastname Schmitz -role Software-Developer -department Software-Engineering-Team -expertise Java -expertise Kotlin -expertise MySQL
+    enter -id 2 -firstname Peter -lastname Schmidt -role Software-Researcher -department Researching-Team -expertise Kotlin -expertise MySQL
+     */
+
+    private static void handleCommandInputs() {
         ConsoleReader.start(input -> {
             final String[] arguments = input.split(" ");
             final ConsoleCommand command = CommandRegistry.find(arguments[0]);
@@ -41,17 +62,13 @@ public class SprintPlanningApplication {
         });
     }
 
-    private static void defaultTablePrint() {
-        final Map<String, List<String>> values = new HashMap<>();
-        values.put("EmployeeId", Arrays.asList("1", "2", "3"));
-        values.put("Firstname", Arrays.asList("Heinz", "Peter", "Ralf"));
-        values.put("LastName", Arrays.asList("Schmitz", "Ulrich", "Horn"));
-        values.put("Department", Arrays.asList("Software-Development", "Planning-Team", "Researching"));
-        values.put("Expertises", Arrays.asList("Java, Kotlin, MySQL", "Product-Owner", "Journalism"));
-        try {
-            TablePrinter.printTable(values);
-        } catch (TablePrinterException e) {
-            e.printStackTrace();
-        }
+    private static void registerShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                EmployeeContainer.getInstance().store();
+            } catch (PersistenceException e) {
+                e.printStackTrace();
+            }
+        }));
     }
 }
